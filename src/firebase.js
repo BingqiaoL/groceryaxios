57b7f748520ddadb232ad44e65308093a8895472
BingqiaoL <49234627+BingqiaoL@users.noreply.github.com>
2020-03-30 22:44:25 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
-
+    apiKey: "AIzaSyC9RZXYzdtyqZXX-OVPMshmO34TZasPNts",
+    authDomain: "bt3103-week8-6fbc1.firebaseapp.com",
+    databaseURL: "https://bt3103-week8-6fbc1.firebaseio.com",
+    projectId: "bt3103-week8-6fbc1",
+    storageBucket: "bt3103-week8-6fbc1.appspot.com",
+    messagingSenderId: "550310774830",
+    appId: "1:550310774830:web:5060173ec4c9aa2a9e37bf",
+    measurementId: "G-K7NCT6R2TJ"
   };
   
   firebase.initializeApp(firebaseConfig);
